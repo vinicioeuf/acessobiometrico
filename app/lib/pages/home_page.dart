1358343d5466699eb2d6f8a6f89e2782f32484c1
Vinicio <59233562+vinicioeuf@users.noreply.github.com>
2023-10-07 21:40:35 -0300

    splash screen + homepage

--- app/lib/pages/home_page.dart
+++ app/lib/pages/home_page.dart
@@ -9,31 +9,106 @@ class HomePage extends StatefulWidget
 }
 class HomePageState extends State<HomePage>
 {
-  int cont = 0;
+  int selectedIndex = 0;
+
   @override
-  Widget build(BuildContext context) 
-  {
-    return Scaffold(
-      appBar: AppBar(title: Text("Página Inicial"),),
-      body: Container(
-        height: 100,
-        width: 100,
-        color: Colors.black,
-        child: Center(
-            child: Container(
-              height: 50,
-              width: 50,
-              color: Colors.red),
-        )
-      ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () { 
-            setState(() {
-              cont++;
-            });
-           },
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: [
+            Image.asset("assets/imagens/labmaker-navbar2.jpg"),
+            Column(
+              children: [
+                buildBox(Icons.rocket_launch, 'PROJETOS', 0),
+                SizedBox(height: 20),
+                buildBox(Icons.report, 'AVISOS', 1),
+                SizedBox(height: 20),
+                buildBox(Icons.date_range, 'AGENDAMENTO', 2),
+                SizedBox(height: 20),
+                buildBox(Icons.fingerprint, 'ACESSOS', 3),
+              ],
+            ),
+          ],
         ),
+        bottomNavigationBar: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Divider(
+              height: 1,
+              color: Colors.black,
+            ),
+            BottomAppBar(
+              child: Container(
+                height: 50,
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    buildIconContainer(Icons.explore, 0),
+                    buildIconContainer(Icons.person, 1),
+                    buildIconContainer(Icons.help_outline, 2),
+                  ],
+                ),
+              ),
+              elevation: 1,
+              color: Colors.grey[300],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildIconContainer(IconData icon, int index) {
+    bool isSelected = selectedIndex == index;
+
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        child: Container(
+          color: isSelected ? Colors.green[700] : Colors.white,
+          child: Icon(
+            icon,
+            color: isSelected ? Colors.white : Colors.green[700],
+            size: 50,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildBox(IconData icon, String text, int index) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.95,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.green[700],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 50,
+          ),
+          SizedBox(width: 10),
+          Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
