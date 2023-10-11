adeb4392fa782136ce27db0d169bedc11e1fc332
Vinicio <59233562+vinicioeuf@users.noreply.github.com>
2023-10-11 16:07:14 -0300

    Update agendamento_page.dart

--- app/lib/pages/agendamento_page.dart
+++ app/lib/pages/agendamento_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 class AgendamentoPage extends StatefulWidget {
   const AgendamentoPage({super.key});
 
@@ -8,8 +8,119 @@ class AgendamentoPage extends StatefulWidget {
 }
 
 class _AgendamentoPageState extends State<AgendamentoPage> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: [
+            Image.asset("assets/imagens/labmaker-navbar2.jpg"),
+            Center(
+              child: RichText(
+                text: TextSpan(
+                  text: 'Área de agendamento',
+                  style: GoogleFonts.oswald(
+                    textStyle: TextStyle(
+                      color: Color.fromARGB(255, 61, 96, 47),
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            
+          ],
+          
+            
+
+        ),
+
+        
+
+
+        bottomNavigationBar: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Divider(
+              height: 1,
+              color: Colors.black,
+            ),
+            BottomAppBar(
+              child: Container(
+                height: 50,
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    buildIconContainer(Icons.explore, 0),
+                    buildIconContainer(Icons.person, 1),
+                    buildIconContainer(Icons.help_outline, 2),
+                  ],
+                ),
+              ),
+              elevation: 1,
+              color: Colors.grey[300],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget buildIconContainer(IconData icon, int index) {
+    bool isSelected = selectedIndex == index;
+
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: isSelected ? Colors.green[700] : Colors.white,
+            borderRadius: BorderRadius.circular(50), // Defina o valor desejado para deixar o Container redondo
+          ),
+          child: Icon(
+            icon,
+            color: isSelected ? Colors.white : Colors.green[700],
+            size: 50,
+          ),
+        ),
+
+      ),
+    );
+  }
+
+  Widget buildBox(IconData icon, String text, int index) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.95,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.green[700],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 50,
+          ),
+          SizedBox(width: 10),
+          Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
