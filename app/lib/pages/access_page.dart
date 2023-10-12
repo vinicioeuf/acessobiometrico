3ec74112487d4f588b0f69c038e98fc0134e4e1e
VictorC78 <victorcarlos0000000000@gmail.com>
2023-10-12 16:39:55 -0300

    .

--- app/lib/pages/access_page.dart
+++ app/lib/pages/access_page.dart
@@ -18,9 +18,12 @@ class _AccessPageState extends State<AccessPage> {
       home: Scaffold(
         body: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // Centraliza verticalmente
             children: [
               SizedBox(height: 35),
-              Image.asset("assets/imagens/labmaker-navbar2.jpg"),
+              Center(
+                child: Image.asset("assets/imagens/labmaker-navbar2.jpg"),
+              ),
               Divider(
                 height: 1,
                 color: Colors.grey,
@@ -40,7 +43,6 @@ class _AccessPageState extends State<AccessPage> {
                 ),
               ),
               SizedBox(height: 20),
-              
               Align(
                 alignment: Alignment.centerRight,
                 child: Padding(
@@ -56,13 +58,17 @@ class _AccessPageState extends State<AccessPage> {
                     child: DropdownButton<String>(
                       value: '12/10/2023',
                       onChanged: (String? newValue) {},
-                      items: <String>['12/10/2023', '11/10/2023', '10/10/2023', '09/10/2023']
-                        .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
+                      items: <String>[
+                        '12/10/2023',
+                        '11/10/2023',
+                        '10/10/2023',
+                        '09/10/2023'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
                       style: TextStyle(color: Colors.black),
                       underline: Container(
                         height: 1,
@@ -76,14 +82,15 @@ class _AccessPageState extends State<AccessPage> {
                   ),
                 ),
               ),
+              SizedBox(height: 20),
               Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center, // Centraliza horizontalmente
                 children: [
                   Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center, // Centraliza horizontalmente
                     children: [
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center, // Centraliza horizontalmente
                         children: [
                           Padding(
                             padding: EdgeInsets.only(left: 16.0),
@@ -99,7 +106,6 @@ class _AccessPageState extends State<AccessPage> {
                               ),
                             ),
                           ),
-                         
                           SizedBox(height: 8.0),
                           Expanded(
                             child: Column(
@@ -107,12 +113,13 @@ class _AccessPageState extends State<AccessPage> {
                               children: [
                                 Text(
                                   'Álvaro Victor',
-                                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                                  style: TextStyle(
+                                      fontSize: 18.0, fontWeight: FontWeight.bold),
                                 ),
-
                                 Text(
                                   'Bolsista',
-                                  style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+                                  style: TextStyle(
+                                      fontSize: 15.0, fontWeight: FontWeight.bold),
                                 ),
                                 SizedBox(height: 5.0),
                                 Row(
@@ -122,48 +129,53 @@ class _AccessPageState extends State<AccessPage> {
                                       children: [
                                         Text(
                                           'Entrou',
-                                          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.green[700]),
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.green[700]),
                                         ),
                                         SizedBox(width: 5.0),
                                         Text(
                                           'ás 09:14',
-                                          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.black),
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.black),
                                         ),
-                                        
                                       ],
                                     ),
                                   ],
                                 ),
-                                 Container(
-                                    child:  ElevatedButton(
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: ElevatedButton(
                                     onPressed: () {
-                                    // Adicione a ação desejada para o botão
+                                      // Adicione a ação desejada para o botão
                                     },
                                     style: ButtonStyle(
-                                      backgroundColor: MaterialStateProperty.all<Color>(Colors.green), // Defina a cor de fundo do botão
+                                      backgroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                              Colors.green), // Defina a cor de fundo do botão
                                     ),
                                     child: Text(
                                       'Ver mais',
-                                      style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),
+                                      style: TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
                                     ),
                                   ),
                                 ),
                               ],
-                              
                             ),
-                            
                           ),
                         ],
                       ),
-
-
                     ],
                   ),
-
-
                   SizedBox(height: 15.0),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center, // Centraliza horizontalmente
                     children: [
                       Padding(
                         padding: EdgeInsets.only(left: 16.0),
@@ -179,7 +191,6 @@ class _AccessPageState extends State<AccessPage> {
                           ),
                         ),
                       ),
-                      
                       SizedBox(height: 8.0),
                       Expanded(
                         child: Column(
@@ -187,12 +198,13 @@ class _AccessPageState extends State<AccessPage> {
                           children: [
                             Text(
                               'Vinicio Eufrazio',
-                              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                              style: TextStyle(
+                                  fontSize: 18.0, fontWeight: FontWeight.bold),
                             ),
-
                             Text(
                               'Bolsista',
-                              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+                              style: TextStyle(
+                                  fontSize: 15.0, fontWeight: FontWeight.bold),
                             ),
                             SizedBox(height: 5.0),
                             Row(
@@ -202,44 +214,50 @@ class _AccessPageState extends State<AccessPage> {
                                   children: [
                                     Text(
                                       'Entrou',
-                                      style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.green[700]),
+                                      style: TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.green[700]),
                                     ),
                                     SizedBox(width: 5.0),
                                     Text(
                                       'ás 08:22',
-                                      style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.black),
+                                      style: TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black),
                                     ),
-                                    
                                   ],
                                 ),
                               ],
                             ),
-                              Container(
-                                child:  ElevatedButton(
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: ElevatedButton(
                                 onPressed: () {
-                                // Adicione a ação desejada para o botão
+                                  // Adicione a ação desejada para o botão
                                 },
                                 style: ButtonStyle(
-                                  backgroundColor: MaterialStateProperty.all<Color>(Colors.green), // Defina a cor de fundo do botão
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.green), // Defina a cor de fundo do botão
                                 ),
                                 child: Text(
                                   'Ver mais',
-                                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
                                 ),
                               ),
                             ),
                           ],
-                          
                         ),
-                        
                       ),
                     ],
                   ),
                 ],
-              )
-
-              
-              
+              ),
             ],
           ),
         ),
@@ -280,73 +298,6 @@ class _AccessPageState extends State<AccessPage> {
           ],
           selectedItemColor: Color.fromARGB(255, 87, 85, 85), // Define a cor dos ícones selecionados
         ),
-      ),
-    );
-  }
-
-  Widget buildIconContainer(IconData icon, int index) {
-    bool isSelected = selectedIndex == index;
-
-    return Expanded(
-      child: InkWell(
-        onTap: () {
-          setState(() {
-            selectedIndex = index;
-          });
-
-          if (index == 2) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => AboutPage()),
-            );
-          } else if (index == 0) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()),
-            );
-          }
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: isSelected ? Colors.green[700] : Colors.white,
-          ),
-          child: Icon(
-            icon,
-            color: isSelected ? Colors.white : Colors.green[700],
-            size: 35,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildBox(IconData icon, String text, int index) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.70,
-      height: 100,
-      decoration: BoxDecoration(
-        color: Colors.green[700],
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            icon,
-            color: Colors.white,
-            size: 50,
-          ),
-          SizedBox(width: 10),
-          Text(
-            text,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ],
       ),
     );
   }
