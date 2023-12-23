815ca8b8065c9f49502bfd22a1a8805caec98798
VictorC78 <victorcarlos0000000000@gmail.com>
2023-12-23 14:37:45 -0300

    .

--- app/lib/pages/team_dev.dart
+++ app/lib/pages/team_dev.dart
@@ -7,7 +7,10 @@ class TeamDevPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.arrow_circle_left_outlined, size: 40,),
+          leading: Icon(
+            Icons.arrow_circle_left_outlined,
+            size: 40,
+          ),
           title: Text(
             "EQUIPE DE DESENVOLVIMENTO",
             style: TextStyle(
@@ -24,23 +27,23 @@ class TeamDevPage extends StatelessWidget {
             shrinkWrap: true,
             padding: EdgeInsets.all(20),
             children: <Widget>[
-              SizedBox(height: 10),
+              SizedBox(height: 25),
               buildTeamMember(
                   'assets/imagens/leoCampello.jpg',
-                  'Leonardo Campello',
-                  'Desenvolvedor Full Stack',
+                  'LEONARDO CAMPELLO',
+                  'Professor',
                   'Mestre em Ciências (UNIVASF). Graduado em Engenharia de Computação (UNIVASF e University of Idaho). Professor EBTT do IFSertãoPE - Campus Salgueiro.'),
-              SizedBox(height: 10),
+              SizedBox(height: 25),
               buildTeamMember(
                   'assets/imagens/vicCarlos.jpg',
-                  'Victor Carlos',
-                  'Desenvolvedor Frontend',
+                  'ÁLVARO VICTOR',
+                  'Aluno',
                   'Graduando em Tecnologia em Sistemas para Internet (IFSertãoPE), Técnico em Informática (IFSertãoPE).'),
-              SizedBox(height: 10),
+              SizedBox(height: 25),
               buildTeamMember(
                   'assets/imagens/viniEufrazio.jpg',
-                  'Vinicio Eufrazio',
-                  'Desenvolvedor Backend',
+                  'VINICIO EUFRAZIO',
+                  'Aluno',
                   'Graduando em Tecnologia em Sistemas para Internet (IFSertãoPE), Técnico em Informática (IFSertãoPE), Administrador de sites na K1Digital.'),
             ],
           ),
@@ -49,11 +52,18 @@ class TeamDevPage extends StatelessWidget {
 
   int selectedIndex = 0;
 
-  Widget buildTeamMember(
-          String imagePath, String name, String role, String description) =>
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
+
+Widget buildTeamMember(
+    String imagePath, String name, String role, String description) {
+  return Container(
+    alignment: Alignment.topCenter,
+    width: double.infinity,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.start, // Alteração aqui
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start, // Alteração aqui
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             CircleAvatar(
@@ -61,24 +71,52 @@ class TeamDevPage extends StatelessWidget {
               backgroundImage: AssetImage(imagePath),
             ),
             SizedBox(height: 10),
-            Text(
-              name,
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 5),
-            Text(
-              role,
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-            SizedBox(height: 5),
-            Text(
-              description,
-              style: TextStyle(fontSize: 16),
-              textAlign: TextAlign.center,
+            Column(
+              children: [
+                Icon(Icons.mail, color: Colors.black),
+                SizedBox(height: 10), // Ajuste no espaçamento
+                Icon(Icons.phone, color: Colors.black),
+              ],
             ),
           ],
         ),
-      );
+        SizedBox(width: 50),
+        Container(
+          width: 1,
+          height: 200,
+          color: Colors.green[800],
+        ),
+        SizedBox(width: 50),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green[800]),
+              ),
+              SizedBox(height: 5),
+              Text(
+                role,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5),
+              Text(
+                description,
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
 
-  void setState(Null Function() param0) {}
 }
