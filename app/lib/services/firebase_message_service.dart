3a65bfb2c0d6f761c8e8159a03d0053a0ca5c361
Vinicio Eufrazio <vinicio.eufrazio@aluno.ifsertao-pe.edu.br>
2023-12-28 13:49:35 -0300

    Update firebase_message_service.dart

--- app/lib/services/firebase_message_service.dart
+++ app/lib/services/firebase_message_service.dart
@@ -1,19 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-
-class FirebaseMessage{
+Future<void> myBackgroundMessageHandler(RemoteMessage message) async {
+    print('Mensagem em segundo plano: ${message.notification?.title}');
+    print('Mensagem em segundo plano: ${message.notification?.body}');
+    // Adicione aqui o código para lidar com a mensagem em segundo plano
+  }
+class FirebaseMessage {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
-  Future<void> initNotifications() async{
+  Future<void> initNotifications() async {
     await _firebaseMessaging.requestPermission();
 
-    final fOMToken = await _firebaseMessaging.getToken();
+    final fcmToken = await _firebaseMessaging.getToken();
 
-    print('token : ${fOMToken}');
+    print('Token: $fcmToken');
+
+    FirebaseMessaging.onBackgroundMessage(myBackgroundMessageHandler);
+
+    FirebaseFirestore.instance
+        .collection('validações') // Substitua 'suaColecao' pelo nome da sua coleção
+        .snapshots()
+        .listen((querySnapshot) {
+      querySnapshot.docChanges.forEach((change) {
+        if (change.type == DocumentChangeType.added) {
+          // Envie a notificação aqui
+          sendNotification();
+        }
+      });
+    });
   }
 
-  // void handleMessage(RemoteMessage? message){
-  //   if(message == null) return;
+  
 
-    
-  // }
+  void sendNotification() {
+    // Adicione o código para enviar a notificação aqui
+  }
 }
