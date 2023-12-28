bc5ffda7b08bd3bea1076be3f41777a2d70a31ff
Vinicio Eufrazio <vinicio.eufrazio@aluno.ifsertao-pe.edu.br>
2023-12-28 19:57:45 -0300

    Update firebase_message_service.dart

--- app/lib/services/firebase_message_service.dart
+++ app/lib/services/firebase_message_service.dart
@@ -1,10 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 Future<void> myBackgroundMessageHandler(RemoteMessage message) async {
-    print('Mensagem em segundo plano: ${message.notification?.title}');
-    print('Mensagem em segundo plano: ${message.notification?.body}');
-    // Adicione aqui o código para lidar com a mensagem em segundo plano
-  }
+  print('Mensagem em segundo plano: ${message.notification?.title}');
+  print('Mensagem em segundo plano: ${message.notification?.body}');
+  // Adicione aqui o código para lidar com a mensagem em segundo plano
+}
 class FirebaseMessage {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
